95addd9f71486f120eb5acd3e27fd374fc8ee45a
Aliz Hammond <randomdude@gmail.com>
2015-04-29 17:25:01 +0100

    Initial commit

---
+++ solarpoller/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace solarpoller
+{
+    class Program
+    {
+        /// <summary>
+        /// This URL exposes some data about current status as XML.
+        /// </summary>
+        private const string location = @"/cgi-bin/coutFile.cgi?fileName=%2Ftmp%2Fsystemdata.xml";
+
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+                Console.Error.WriteLine("Usage: solarpoller <IP>");
+
+            // Parse our IP address argument to ensure it is a valid IP. Handily prevents injections into the URL we
+            // use later on.
+            IPAddress addy;
+            if (!IPAddress.TryParse(args[0], out addy))
+                Console.Error.WriteLine("Bad IP address '" + args[0] + "'");
+
+            // Try three times. If they all fail, print -1 and exit with a status code of -1.
+            int retries = 3;
+            while (retries > 0)
+            {
+                try
+                {
+                    WebRequest req = WebRequest.Create("http://" + addy.ToString() + location);
+
+                    WebResponse resp = req.GetResponse();
+
+                    string respString;
+                    using (StreamReader respStream = new StreamReader(resp.GetResponseStream()))
+                    {
+                        respString = respStream.ReadToEnd();
+                    }
+
+                    // We don't even need to bother processing the XML fully. Just pull the number out
+                    // of this tag.
+                    string startToken = @"<PAC unit=""W"" class=""power"">";
+                    string endToken = @"</PAC>";
+
+                    int startPos = respString.IndexOf(startToken) + startToken.Length;
+                    int endPos = respString.IndexOf(endToken);
+                    string fishedOut = respString.Substring(startPos, endPos - startPos);
+
+                    int num = int.Parse(fishedOut);
+
+                    Console.WriteLine(num);
+                    return 0;
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Exception: " + e.Message);
+
+                    retries--;
+                }
+            }
+
+            Console.WriteLine("-1");
+            return -1;
+        }
+    }
+}
---
+++ solarpoller/readme.txt
@@ -0,0 +1,5 @@
+﻿A really simple C# project to pull current power use out of a Fronius Datalogger Web (http://www.fronius.com/cps/rde/xchg/SID-4392605D-A845710A/fronius_international/hs.xsl/83_16106_ENG_HTML.htm) and return it on the commandline.
+
+Feed it the IP of your datalogger and it will return -1 on failure and 0 on success.
+
+It is super-simple and straightforward.
